04cecf6814c3c82cc8db84b612720bde0b604de9
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-05-28 12:22:28 +0300

    feat: Add qucik link on startup

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import app from "./app"
 const port = 3000
 
 app.listen(port, () => {
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on http://localhost:${port}`)
 })
