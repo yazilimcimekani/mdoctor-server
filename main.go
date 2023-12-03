e330f61bf00d76391088d175771d4d994cd266c8
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-12-03 23:35:02 +0300

    feat: Finalize the folder strcture and http server

--- main.go
+++ main.go
@@ -4,51 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/yazilimcimekani/mdoctor-server/pkg/controllers"
+	"github.com/yazilimcimekani/mdoctor-server/pkg/env"
 )
 
 func main() {
-	// Define the handler for the /public/ route
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-
-	// Define the handler for the markdown route
-	http.HandleFunc("/markdown", markdown())
-
-	// Get the port from the environment or use the default port of 8080
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	// Load the .env file
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
 	}
+	port := env.GetEnv("PORT", "8080")
+
+	// Define the handlers
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	http.HandleFunc("/markdown", controllers.Markdown())
 
 	// Start the server on given port
 	startMessage := fmt.Sprintf("Server started on http://localhost:%s", port)
 	log.Println(startMessage)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-func loadFile(htmlFilePath string) string {
-	htmlContent, err := os.ReadFile(htmlFilePath)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(htmlContent)
-}
-
-func mdToHtml(md string) string {
-	if md == "# Hello World" {
-		md = fmt.Sprintf(`<h1>Hello World</h1>`)
-	}
-	return md
-}
-
-func markdown() func(w http.ResponseWriter, r *http.Request) {
-	const mdTemplatePath = "views/md.html"
-	var html string = loadFile(mdTemplatePath)
-	fullHTML := fmt.Sprintf(html, mdToHtml("# Hello World"))
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(fullHTML))
-	}
 }
